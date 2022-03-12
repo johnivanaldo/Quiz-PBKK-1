213c50915f0c346681f5ed32537af0a6b7d2d98b
johnivanaldo <36225278+johnivanaldo@users.noreply.github.com>
2022-03-12 20:47:37 +0700

    Add files via upload

---
+++ Controllers/q1Controller.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Endorsin.Controllers
+{
+    public class q1Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult profile()
+        {
+            return View();
+        }
+
+        public IActionResult company()
+        {
+            return View();
+        }
+
+        public IActionResult service()
+        {
+            return View();
+        }
+
+        public IActionResult collaboration()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Endorsin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
